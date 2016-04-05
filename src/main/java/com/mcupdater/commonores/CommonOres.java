accf7ff34380cc4e9d8b5f53ff25f062bf95a622
Ammon Lauritzen <allaryin@gmail.com>
2016-04-05 13:46:27 -0500

    Added stub mod file.

---
+++ src/main/java/com/mcupdater/commonores/CommonOres.java
@@ -0,0 +1,14 @@
+package com.mcupdater.commonores;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = "commonores", useMetadata = true)
+public class CommonOres {
+
+	@EventHandler
+	public void init(FMLInitializationEvent ev) {
+
+	}
+}
