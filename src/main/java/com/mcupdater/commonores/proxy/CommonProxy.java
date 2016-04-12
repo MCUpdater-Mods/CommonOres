63897786bb0a56eaf150a059a69dea5b866eac64
Scott M. Barbour <smbarbour@gmail.com>
2016-04-11 20:34:12 -0500

    Fancy reflection code not actually required.

--- src/main/java/com/mcupdater/commonores/proxy/CommonProxy.java
+++ src/main/java/com/mcupdater/commonores/proxy/CommonProxy.java
@@ -1,9 +1,7 @@
 package com.mcupdater.commonores.proxy;
 
 import com.mcupdater.commonores.util.LanguageBuilder;
-import net.minecraft.util.text.translation.I18n;
 import net.minecraft.util.text.translation.LanguageMap;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 import java.io.ByteArrayInputStream;
 
@@ -24,6 +22,6 @@ public class CommonProxy {
 	}
 
 	public void injectLocalizations() {
-		((LanguageMap) ReflectionHelper.getPrivateValue(I18n.class, new I18n(), "localizedName")).inject(new ByteArrayInputStream(localizations.getBytes()));
+		LanguageMap.inject(new ByteArrayInputStream(localizations.getBytes()));
 	}
 }
