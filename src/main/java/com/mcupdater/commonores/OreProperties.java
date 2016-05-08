6020b0c28e100e79f2226a7389430106e547bcac
Scott M. Barbour <smbarbour@gmail.com>
2016-05-08 04:16:21 -0500

    A little prep for configs

---
+++ src/main/java/com/mcupdater/commonores/OreProperties.java
@@ -0,0 +1,9 @@
+package com.mcupdater.commonores;
+
+public class OreProperties {
+	int minY;
+	int maxY;
+	int minVeinSize;
+	int maxVeinSize;
+	int[] dimensions;
+}
---
+++ src/main/java/com/mcupdater/commonores/Resource.java
@@ -0,0 +1,11 @@
+package com.mcupdater.commonores;
+
+public class Resource {
+	String name;
+	String oreDictNames;
+	int miningLevel;
+	OreProperties oreProperties;
+	int density;
+	int color;
+	int enchantability;
+}
